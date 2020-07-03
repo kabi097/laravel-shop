db4a8e782ad1c061a15e76e5d98fa8fb348c2493
kabi097 <paweljanwiczynski@gmail.com>
2020-07-03 23:14:18 +0200

    Cart view

--- public/js/app.js
+++ public/js/app.js
@@ -37227,6 +37227,8 @@ module.exports = function(module) {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+__webpack_require__(/*! ./cart */ "./resources/js/cart.js");
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
@@ -37271,6 +37273,24 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+/***/ }),
+
+/***/ "./resources/js/cart.js":
+/*!******************************!*\
+  !*** ./resources/js/cart.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+$(document).ready(function () {
+  var cart = $("#cart");
+  cart.hide();
+  $("#cart-button").click(function () {
+    cart.toggle();
+  });
+  $(".product");
+});
 
 /***/ }),
 
---
+++ resources/js/cart.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var cart = $("#cart");
+    cart.hide();
+    $("#cart-button").click(function () {
+        cart.toggle();
+    });
+
+    $(".product")
+});
