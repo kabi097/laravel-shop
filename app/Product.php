6fbf9ad2e77d9010817712a2b3bb1f1a77525a80
kabi097 <paweljanwiczynski@gmail.com>
2020-08-15 23:46:43 +0200

    fix: order products by created_at by default

--- app/Product.php
+++ app/Product.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use TCG\Voyager\Traits\Resizable;
 
 class Product extends Model
@@ -10,6 +11,14 @@ class Product extends Model
     use Resizable;
 
     protected $fillable = ['title', 'price', 'description', 'quantity'];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('age', function (Builder $builder) {
+            $builder->orderByDesc('created_at');
+        });
+    }
+
 
     public function category() {
         return $this->belongsTo(Category::class);
